4ef4e3c923eb505db19e706637cec96cb3d4ecc0
Christina Brown <chrissy@christinabrown.co.uk>
2021-02-13 15:30:27 +0000

    basic layout and background images for dynamic background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
 
+const API = {
+  // eslint-disable-next-line no-undef
+  key: process.env.REACT_APP_WEATHER_WIDGET,
+  base: "https://api.openweathermap.org/data/2.5",
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <main>
+        <div className="search">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search a city..."
+          />
+        </div>
+        <div className="city-info">
+          <div className="location">London, GB</div>
+          <div className="date"></div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
